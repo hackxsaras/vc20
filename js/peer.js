40122a0220d64744063f1d9434cb4cfebabc8cd4
unknown <hackxsaras@gmail.com>
2023-09-08 16:01:58 +0530

    grid and colors

--- js/peer.js
+++ js/peer.js
@@ -59,42 +59,42 @@ class Model {
         var x1 = inst.peers[peer1].position.x, y1 = inst.peers[peer1].position.y,
             x2 = inst.peers[peer2].position.x, y2 = inst.peers[peer2].position.y;
 
-        return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
+        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
     changePosition(x, y) {
-        var change = {position:{x, y}};
+        var change = { position: { x, y } };
         this.updatePeer(this.peer.id, change);
         this.sendToServer({
             func: "changeData",
             args: change
         })
-        for(var key in this.peers) {
+        for (var key in this.peers) {
             this.updatePeer(key, {});
         }
     }
     updatePeer(id, data) {
-        if(!this.peers[id])
+        if (!this.peers[id])
             this.peers[id] = data;
-        for(var key in data){
+        for (var key in data) {
             this.peers[id][key] = data[key];
         }
 
         this.peers[id].distance = this.distance(this.peer.id, id);
 
-        if(this.peers[id].distance > 4) {
+        if (this.peers[id].distance > 4) {
             this.disconnectCall("out", id); // disconnect this call
-        } 
-        else if(this.initiatedCalls[id] && !this.outCalls[id]) {
+        }
+        else if (this.initiatedCalls[id] && !this.outCalls[id]) {
             this.call(id); // reconnect again
         }
-        
-        this.dispatch("peer-update", id, this.peers[id]);
+
+        this.dispatch("peerUpdated", id, this.peers[id]);
     }
     setList(list) { // just reset all the calls
         for (var id in list) {
             this.updatePeer(id, list[id]);
         }
-        this.dispatch("list-update", this.peers);
+        this.dispatch("listUpdated", this.peers);
         this.initiateCalls();
     }
 
@@ -124,7 +124,9 @@ class Model {
 
     initiateCalls() {
         for (var peer in this.peers) {
-            this.call(peer, { new: true });
+            if(this.distance(peer, this.peer.id) < 4){
+                this.call(peer, { new: true });
+            }
         }
         this.initiatedCalls[peer] = true;
     }
@@ -140,7 +142,7 @@ class Model {
         }
         deb.g("Calling", peer, "with", meta);
         if (!this.outStream) {
-            this.dispatch("request-stream").then(function (stream) {
+            this.dispatch("requestStream").then(function (stream) {
                 inst.outStream = stream;
                 inst.callUtil(peer, meta);
             });
@@ -150,7 +152,7 @@ class Model {
     }
     updateInStream(peer, stream) {
         this.inStreams[peer] = stream;
-        this.dispatch("stream-update", peer, stream);
+        this.dispatch("streamUpdated", peer, stream);
     }
     answerCall(call) {
         var inst = this;
@@ -166,14 +168,14 @@ class Model {
     }
     disconnectCall(type, peer) {
         if (type == "in") {
-            if(this.inCalls[peer]) {
+            if (this.inCalls[peer]) {
                 deb.r("Disconnecting Call", type, "from", peer);
                 this.inCalls[peer]?.close();
                 delete this.inCalls[peer];
                 this.updateInStream(peer, null);
             }
         } else {
-            if(this.outCalls[peer]) {
+            if (this.outCalls[peer]) {
                 deb.r("Disconnecting Call", type, "from", peer);
                 this.outCalls[peer]?.close();
                 delete this.outCalls[peer];
@@ -183,7 +185,7 @@ class Model {
     sendToServer(message) {
         this.serverConnection.send(message);
     }
-    handleServerData(conn, message){
+    handleServerData(conn, message) {
         var inst = this;
         var functions = {
             setList(conn, args) {
@@ -193,7 +195,7 @@ class Model {
                 inst.updatePeer(args.peer, args.data);
             }
         };
-        deb.b("Server("+conn.peer+") says", message.func, message.args);
+        deb.b("Server(" + conn.peer + ") says", message.func, message.args);
         functions[message.func].call(this, conn, message.args);
     }
 }
@@ -204,13 +206,28 @@ class View {
         this.peers = {};
         this.audios = {};
         this.distances = {};
+
+        this.listeners = {};
+
+        this._initLocalListeners();
+    }
+    _initLocalListeners() {
+        var inst = this;
+        this.playground.addEventListener("dblclick", function (event) {
+            if (event.target.id == "playground") {
+                var x = event.pageX, y = event.pageY;
+                x = parseInt(x / 50);
+                y = parseInt(y / 50);
+                inst.dispatch("changePosition", x, y);
+            }
+        })
     }
     playAudio(peer, stream) {
         if (!this.audios[peer]) {
             this.audios[peer] = document.createElement("audio");
             document.body.appendChild(this.audios[peer]);
         }
-        if( stream == null) {
+        if (stream == null) {
             // delete this audio
             this.audios[peer].parentNode.removeChild(this.audios[peer]);
             delete this.audios[peer];
@@ -220,12 +237,12 @@ class View {
         this.updateVolume(peer);
         this.audios[peer].play();
     }
-    updateVolume(peer){
-        if(this.audios[peer]){
+    updateVolume(peer) {
+        if (this.audios[peer]) {
             let d = this.distances[peer];
             let v = 1 - 0.25 * d; // cannot here after 4 meters // maximum limit
             v = Math.max(v, 0);
-            deb.g("Altering volume for peer"+peer+": "+v);
+            deb.g("Altering volume for peer:" + peer + ": " + v);
             this.audios[peer].volume = v;
         }
     }
@@ -234,13 +251,23 @@ class View {
             var el = document.createElement("div");
             el.innerHTML = peer;
             el.classList.add("vc-peer");
+
             this.playground.appendChild(el);
             this.peers[peer] = el;
+
+            var r = Math.random() * 255;
+            var g = Math.random() * 255;
+            var b = Math.random() * 255;
+            console.log(r, g, b);
+            var tc = (r + g + b >= 382.5) ? "#000" : "#fff";
+            this.peers[peer].style.background = `rgb(${r}, ${g}, ${b})`;
+            this.peers[peer].style.color = tc;
         }
-        this.peers[peer].style.top = (data.position.y*50)+"px";
-        this.peers[peer].style.left = (data.position.x*50)+"px";
+        this.peers[peer].style.top = (data.position.y * 50) + "px";
+        this.peers[peer].style.left = (data.position.x * 50) + "px";
         this.peers[peer].innerHTML = peer;
-        
+
+
         this.distances[peer] = data.distance;
 
         this.updateVolume(peer);
@@ -257,17 +284,29 @@ class View {
         });
         return stream;
     }
+
+    // Providing events
+    on(fn, callback) {
+        this.listeners[fn] = callback;
+    }
+    dispatch(fn, ...args) {
+        if (this.listeners[fn]) {
+            return this.listeners[fn](...args);
+        }
+    }
 }
 class Controller {
     constructor(id) {
-
+        this.id = id;
         this.model = new Model(id);
         this.view = new View(id);
 
-        this.model.on("list-update", this.onListUpdated.bind(this));
-        this.model.on("peer-update", this.onPeerUpdated.bind(this));
-        this.model.on("request-stream", this.onRequestStream.bind(this));
-        this.model.on("stream-update", this.onUpdateStream.bind(this));
+        this.model.on("listUpdated", this.onListUpdated.bind(this));
+        this.model.on("peerUpdated", this.onPeerUpdated.bind(this));
+        this.model.on("streamUpdated", this.onStreamUpdated.bind(this));
+        this.model.on("requestStream", this.onRequestStream.bind(this));
+
+        this.view.on("changePosition", this.handleChangePosition.bind(this));
     }
     onListUpdated(list) {
         this.view.displayList(list);
@@ -275,11 +314,17 @@ class Controller {
     onPeerUpdated(peer, data) {
         this.view.displayPeer(peer, data);
     }
+    onUpdatePeer(data) {
+        this.model.updatePeer(this.id, data);
+    }
     onRequestStream() {
         return this.view.requestStream();
     }
-    onUpdateStream(peer, stream) {
+    onStreamUpdated(peer, stream) {
         this.view.playAudio(peer, stream);
+    }
+    handleChangePosition(x, y) {
+        this.model.changePosition(x, y);
     }
 }
 
